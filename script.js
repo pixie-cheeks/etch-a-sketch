2d3efdcc02b256a3c23940f635180b0953131055
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2023-08-05 12:33:12 +0530

    Add a slider that changes the div squares per side

--- script.js
+++ script.js
@@ -1,25 +1,80 @@
-const container = document.createElement('div');
-container.classList = 'container';
-
-for (i = 0; i < 16; i++) {
-	const columnWrapper = document.createElement('div');
-	columnWrapper.classList = 'column-wrapper';
-	for (j = 0; j < 16; j++) {
-		const square = document.createElement('div');
-		square.classList = 'square';
-		columnWrapper.append(square);
+function gridDimensions(squaresPerSide) {
+	for (i = 0; i < squaresPerSide; i++) {
+		const columnWrapper = document.createElement('div');
+		columnWrapper.classList = 'column-wrapper';
+		for (j = 0; j < squaresPerSide; j++) {
+			const square = document.createElement('div');
+			square.classList = 'square';
+			columnWrapper.append(square);
+		}
+		container.append(columnWrapper);
 	}
-	container.append(columnWrapper);
 }
 
-document.body.append(container);
+function setEventListeners() {
+	const columnWrappers = document.querySelectorAll('.column-wrapper');
+	columnWrappers.forEach(squareDiv => {
+		squareDiv.addEventListener('mouseover', hoverEffect);
+	});
+}
+
+function removeGrid() {
+	const columns = document.querySelectorAll('.column-wrapper');
+	columns.forEach(element => element.remove());
+}
+
+function changeGrid(squaresPerSide) {
+	removeGrid();
+	gridDimensions(squaresPerSide);
+	setEventListeners();
+}
 
 function hoverEffect(event) {
 	event.target.classList.add('hover');
 }
 
-const rowWrappers = document.querySelectorAll('.column-wrapper');
-rowWrappers.forEach(squareDiv => {
-	squareDiv.addEventListener('mouseover', hoverEffect);
-	// squareDiv.addEventListener('mouseleave', removeHoverEffect);
+function manageSlider(event) {
+	sliderText.value = event.target.value;
+	let argument = parseInt(event.target.value);
+	if (validationText.textContent === '') {
+		changeGrid(argument);
+	}
+}
+
+function inputValidation(event) {
+	if (isNaN(+event.target.value)) {
+		validationText.textContent = 'Only numbers are allowed.';
+	} else if (event.target.value === '') {
+		slider.value = event.target.value = 1;
+		changeGrid(1);
+	} else if (+event.target.value < 1 || +event.target.value > 100) {
+		validationText.textContent = 'Only numbers between 1 & 100 are allowed.';
+	} else {
+		validationText.textContent = '';
+	}
+}
+
+const container = document.createElement('div');
+container.classList = 'container';
+gridDimensions(16);
+document.body.append(container);
+setEventListeners();
+
+const slider = document.querySelector('input[type="range"]');
+const sliderText = document.querySelector('.text-input');
+const validationText = document.querySelector('.input-validation');
+
+
+slider.addEventListener('input', manageSlider);
+sliderText.addEventListener('input', inputValidation);
+sliderText.addEventListener('blur', event => {
+	slider.value = event.target.value;
+	changeGrid(slider.value);
 });
+sliderText.addEventListener('keydown', event => {
+	if (event.key === 'Enter') {
+		slider.value = event.target.value;
+		changeGrid(slider.value);
+		sliderText.blur();
+	}
+})
